ed51fe92f086e0f51573cc629a3d10dfc272b49e
Hao Ye <lhopitalified@gmail.com>
2023-11-21 11:33:54 -0500

    fix documentation

--- R/api-functions.R
+++ R/api-functions.R
@@ -16,15 +16,18 @@ do_auth <- function()
 #' This function expects that there is an encrypted file in RDS format with an
 #' OAuth token, and an environmental variable with the key to decrypt the token.
 #'
+#' @param token_file filepath to the encrypted token file
+#' @param decrypt_env_var name of the environmental variable that is the decryption key
+#'
 #' @return an OAuth token
 #' @export
-decrypt_gdrive_token <- function(token_RDS = ".secrets/gdrive-token.rds",
+decrypt_gdrive_token <- function(token_file = ".secrets/gdrive-token.rds",
                                  decrypt_env_var = "GDRIVE_KEY")
 {
     token <- NULL
     if (gargle::secret_has_key(decrypt_env_var))
     {
-        token <- gargle::secret_read_rds(token_RDS,
+        token <- gargle::secret_read_rds(token_file,
                                 key = decrypt_env_var)
     }
 
